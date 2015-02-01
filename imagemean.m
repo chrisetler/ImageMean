d367a42d6034f46d7baa610587c9c40e421d5695
David Etler <davidretler@gmail.com>
2015-02-01 13:52:20 -0500

    Vectorizing code for efficiency.

--- imagemean.m
+++ imagemean.m
@@ -4,57 +4,57 @@
 %value for each block as the output color for that section of the image
 %
 %Essentially this is an image-pixelator
+clear;
+image_name = '1.png';
 
-
-a = imread('1.png');
+a = imread(image_name);
 % width and height
 % z is the number of channels. Right now 3 is hardcoded in but that could
 % be easily changed to 'z' in the near future to allow for grayscale alpha
 % images to work.
 [y,x,z] = size(a);
 
-%generates the output file
-%uint8 because color values go typically from 0-255
-aout = uint8(zeros(y,x,3));
+
 
 %how big of an area to break the image up into
 %bigger => blockier image
 boxsize = 10;
-
+num_blocks_n = ceil(x / boxsize);
+num_blocks_m = ceil(y / boxsize);
+%generates the output file
+%uint8 because color values go typically from 0-255
+aout = uint8(zeros(num_blocks_m,num_blocks_n,3));
 
 %goes through the image block by block
-for n = 1:boxsize:x;
-    for m = 1:boxsize:y;
-
+for n = 1:(num_blocks_n);
+    for m = 1:(num_blocks_m);
+        n_offset = (n-1)*boxsize+1;
+        m_offset = (m-1)*boxsize+1;
 
         %crops the image to a square with side lengths of $blocksize
-        b = imcrop(a, [n m n+boxsize m+boxsize]);
+        %b = imcrop(a, [n m n+boxsize m+boxsize]);
 
 
         %get the mean for each channel in the box
-        sum = uint64(zeros(1,3));
+        channel_sum = uint64(zeros(1,3));
         len = zeros(1,3);
         mean = zeros(1,3);
         for channel = 1:1:3;
-            for i = 1:1:boxsize;
-                for j = 1:1:boxsize;
-                    if (i+n) <= x && (j+m)<= y;
-                        sum(channel) = sum(channel) + uint64(b(j,i,channel));
-                        len(channel) = len(channel) +1;
-                    end
-                end
+            if(n ~= num_blocks_n && m ~= num_blocks_m)
+                channel_sum(channel) = sum(sum(a((m_offset):(m_offset+boxsize), (n_offset):(n_offset+boxsize), channel)));
+                channel_length = boxsize*boxsize;
+            else
+                channel_sum(channel) = sum(sum(a((m_offset):y, (n_offset):x, channel)));
+                channel_length = (y - m_offset)*(x - n_offset);
+     
             end
-            mean(channel) = sum(channel) / len(channel);
+            mean(channel) = channel_sum(channel) / channel_length;
         end
 
         %produce an image using only the mean value
         
-        for channel = 1:1:3;
-            for i = 1:1:boxsize;
-                for j = 1:1:boxsize;
-                    aout(m+j-1,n+i-1,channel) = uint8(mean(channel));
-                end
-            end
+        for channel = 1:1:3;   
+            aout(m,n,channel) = uint8(mean(channel));
         end
     end
 end
